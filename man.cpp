f4d10cd472cc34675ec7fee63743de11f3807b0c
YourSteBan <74486898+YourSteBan@users.noreply.github.com>
2024-04-27 15:45:53 +0500

    egor

---
+++ man.cpp
@@ -0,0 +1,12 @@
+#include "graphics.h"
+#include "picture.hpp"
+
+void main()
+{
+   setfillstyle(1, WHITE);
+   fillellipse(600, 300, 50, 50);
+   bar(580, 345, 620, 400);
+   bar(580, 400, 590, 500);
+   bar(610, 400, 620, 500);
+   bar(450, 350, 750, 360);
+}
